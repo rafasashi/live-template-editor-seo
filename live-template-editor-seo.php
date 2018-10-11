75f3fbec4421c605913dbf89815b27d3dd872eb1
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2018-10-11 17:26:05 +0530

    check ltple client exists

--- live-template-editor-seo.php
+++ live-template-editor-seo.php
@@ -24,60 +24,68 @@
 	
 	if ( ! defined( 'ABSPATH' ) ) exit;
 	
-	/**
-	 * Returns the main instance of LTPLE_SEO to prevent the need to use globals.
-	 *
-	 * @since  1.0.0
-	 * @return object LTPLE_SEO
-	 */
-	function LTPLE_SEO ( $version = '1.0.0' ) {
+	if( function_exists('LTPLE_Client') ){
 		
-		$instance = LTPLE_Client::instance( __FILE__, $version );
-		
-		if ( empty( $instance->seo ) ) {
+		/**
+		 * Returns the main instance of LTPLE_SEO to prevent the need to use globals.
+		 *
+		 * @since  1.0.0
+		 * @return object LTPLE_SEO
+		 */
+		function LTPLE_SEO ( $version = '1.0.0' ) {
 			
-			$instance->seo = new stdClass();
+			$instance = LTPLE_Client::instance( __FILE__, $version );
 			
-			$instance->seo = LTPLE_SEO::instance( __FILE__, $instance, $version );
-		}
+			if ( empty( $instance->seo ) ) {
+				
+				$instance->seo = new stdClass();
+				
+				$instance->seo = LTPLE_SEO::instance( __FILE__, $instance, $version );
+			}
 
-		return $instance;
-	}	
-	
-	add_filter( 'plugins_loaded', function(){
+			return $instance;
+		}	
+		
+		add_filter( 'plugins_loaded', function(){
 
-		$dev_ip = '109.28.69.143';
-		
-		$mode = ( ($_SERVER['REMOTE_ADDR'] == $dev_ip || ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] == $dev_ip )) ? '-dev' : '');
-		
-		if( $mode == '-dev' ){
+			$dev_ips = array();
 			
-			ini_set('display_errors', 1);
-		}
+			if( defined('MASTER_ADMIN_IPS') ){
+				
+				$dev_ips = MASTER_ADMIN_IPS;
+			}
+			
+			$mode = ( ( in_array( $_SERVER['REMOTE_ADDR'], $dev_ips ) || ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) && in_array( $_SERVER['HTTP_X_FORWARDED_FOR'], $dev_ips ) )) ? '-dev' : '');
+			
+			if( $mode == '-dev' ){
+				
+				ini_set('display_errors', 1);
+			}
 
-		// Load plugin functions
-		require_once( 'includes'.$mode.'/functions.php' );	
-		
-		// Load plugin class files
+			// Load plugin functions
+			require_once( 'includes'.$mode.'/functions.php' );	
+			
+			// Load plugin class files
 
-		require_once( 'includes'.$mode.'/class-ltple.php' );
-		require_once( 'includes'.$mode.'/class-ltple-settings.php' );
+			require_once( 'includes'.$mode.'/class-ltple.php' );
+			require_once( 'includes'.$mode.'/class-ltple-settings.php' );
 
-		// Autoload plugin libraries
+			// Autoload plugin libraries
+			
+			$lib = glob( __DIR__ . '/includes'.$mode.'/lib/class-ltple-*.php');
+			
+			foreach($lib as $file){
+				
+				require_once( $file );
+			}
 		
-		$lib = glob( __DIR__ . '/includes'.$mode.'/lib/class-ltple-*.php');
-		
-		foreach($lib as $file){
-			
-			require_once( $file );
-		}
-	
-		if( $mode == '-dev' ){
-			
-			LTPLE_SEO('1.1.1');
-		}
-		else{
-			
-			LTPLE_SEO('1.1.0');
-		}		
-	});
+			if( $mode == '-dev' ){
+				
+				LTPLE_SEO('1.1.1');
+			}
+			else{
+				
+				LTPLE_SEO('1.1.0');
+			}		
+		});
+	}
